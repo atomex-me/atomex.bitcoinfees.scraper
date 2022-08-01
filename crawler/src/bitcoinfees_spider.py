3511a3f9b4a2f9635b8f9ef7f501dc47ec30c638
Konstantin <johnysalvesen@gmail.com>
2022-08-01 09:48:34 +0300

    Added user-agent;

--- crawler/src/bitcoinfees_spider.py
+++ crawler/src/bitcoinfees_spider.py
@@ -7,6 +7,15 @@ class BitcoinFeesSpider(scrapy.Spider):
         'https://bitcoinfees.net/',
     ]
 
+    custom_settings = {
+        USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+
+        DEFAULT_REQUEST_HEADERS = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'en',
+        }
+    }
+
     def parse(self, response):
         fee_rates_table_body = response.css('tbody')
         for fee_rate_row in fee_rates_table_body.css('tr'):
