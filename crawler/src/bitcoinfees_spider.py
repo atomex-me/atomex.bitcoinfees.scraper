77decc999a4771356d77acbc8d349f7dc2b5db38
Konstantin <johnysalvesen@gmail.com>
2022-08-04 11:55:46 +0200

    Added user-agent headers to Crawler;

--- crawler/src/bitcoinfees_spider.py
+++ crawler/src/bitcoinfees_spider.py
@@ -8,9 +8,9 @@ class BitcoinFeesSpider(scrapy.Spider):
     ]
 
     custom_settings = {
-        USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
 
-        DEFAULT_REQUEST_HEADERS = {
+        'DEFAULT_REQUEST_HEADERS': {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en',
         }
